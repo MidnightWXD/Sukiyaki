9329c852a097d81973b0000fbc66d743750fda0d
Yuchen Cui <yuchencui@hotmail.com>
2022-05-28 11:25:20 -0400

    Fix bug, unpdate readme file

--- Assets/js/JavaScript.js
+++ Assets/js/JavaScript.js
@@ -1,55 +1,33 @@
-// id=currentday show current day
+//show current day
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 var container = $(".container");
-var inputGroupTitle = $('<p>');
-
-
-function setTime() {
-    var time = moment().format("h");
-    var timeArray = ["9", "10", "11", "12", "1", "2", "3", "4", "5"];
-    for (var i = 0; i < timeArray.length; i++) {
-        if(inputGroupTitle.text().include("AM")){
-            if(timeArray[i] < time){
-                inputGroupTitle.addClass("past");
-            }else if(timeArray[i] === time){
-                inputGroupTitle.addClass("present");
-            }
-            else{
-                inputGroupTitle.addClass("future");
-            }
-        }else{
-            if(timeArray[i] < time){
-                inputGroupTitle.addClass("past");
-            }else if(timeArray[i] === time){
-                inputGroupTitle.addClass("present");
-            }
-            else{
-                inputGroupTitle.addClass("future");
-            }
-        }
-    }
-}
-
-
+//set time for each input
+var timeArray = [
+    ["9","AM"],
+    ["10","AM"],
+    ["11","AM"],
+    ["12","PM"],
+    ["1","PM"],
+    ["2","PM"],
+    ["3","PM"],
+    ["4","PM"],
+    ["5","PM"]
+];
+//function to set input bar on the page
 function setInputGroup() {
-    var timeArray = ["9", "10", "11", "12", "1", "2", "3", "4", "5"];
     for (var i = 0; i < timeArray.length; i++) {                  
-        var currentTime = timeArray[i];
+        var setTagTime = timeArray[i][0] + timeArray[i][1];
         var inputGroup = $('<div>');
         inputGroup.addClass("input-group"); 
         var inputGroupTitle = $('<p>');       
         inputGroupTitle.addClass("input-group-text");
         inputGroupTitle.attr("id", "basic-addon1"); 
-        if(timeArray[i] <= 12) {
-            inputGroupTitle.text(currentTime + "AM");
-        } else {
-            inputGroupTitle.text(currentTime + "PM");
-        }      
+        inputGroupTitle.text(setTagTime);    
         var inputGroupInput = $('<input>');
-        //
+        //class for targetting saved text
         inputGroupInput.addClass(function(){
-            var time = timeArray[i];
+            var time = timeArray[i][0];
             return "form-control " + time;
         });
         inputGroupInput.attr("type", "text");
@@ -61,18 +39,58 @@ function setInputGroup() {
         inputGroupButtonButton.addClass("btn btn-outline-secondary");
         inputGroupButtonButton.attr("type", "button");
         inputGroupButtonButton.attr("id", "button-addon2");
-        inputGroupButtonButton.text("Save");
+        var icon = $('<i>');
+        icon.addClass("fas fa-save");
         container.append(inputGroup);
-        inputGroup.append(inputGroupTitle);
-        inputGroup.append(inputGroupInput);
+        inputGroup.append(inputGroupTitle);       
         inputGroup.append(inputGroupButton);
+        inputGroupButton.append(inputGroupInput);
         inputGroupButton.append(inputGroupButtonButton);
-
+        inputGroupButtonButton.append(icon);
+        // if statement to set color of input bar
+        var currentHour = parseInt(moment().format("H"));
+        if(timeArray[i][0] > 5){
+            if(timeArray[i][0] == currentHour){
+                inputGroupInput.addClass("present");
+            }else if(timeArray[i][0] > currentHour){
+                inputGroupInput.addClass("future");
+            }else{
+                inputGroupInput.addClass("past");
+                console.log(timeArray[i][0] + " " + currentHour);            
+            }
+        }else{
+            const transferTime = parseInt(timeArray[i][0]) + 12;
+            if(transferTime == currentHour){
+                inputGroupInput.addClass("present");
+            }else if(transferTime < currentHour){
+                inputGroupInput.addClass("past");
+            }else{
+                inputGroupInput.addClass("future");
+            }                        
+        }
+        //load text from local storage
+        loadText();        
     }
 }
 
-
-
-
 setInputGroup();
-setTime();
+
+//jquery to save text to local storage
+$(".btn").on("click", function(event){
+    event.preventDefault();
+    var text = $(this).siblings("input").val();
+    var inputClass = $(this).siblings("input").attr("class");
+    var array = inputClass.split(" ");
+    var time = array[1];
+    localStorage.setItem(time, text);
+});
+//function to load text from local storage
+function loadText(){
+    for (var i = 0; i < timeArray.length; i++) {
+        var time = timeArray[i][0];
+        var text = localStorage.getItem(time);
+        if(text){
+            $(".form-control." + time).val(text);
+        }
+    }
+}
